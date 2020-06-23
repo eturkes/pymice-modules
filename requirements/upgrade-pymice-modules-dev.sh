bba2dc5237411e24896c99c76c98ec13e2743b32
Emir Turkes <eturkes@bu.edu>
2020-06-23 07:13:49 -0400

    Add institution to copyright

--- requirements/upgrade-pymice-modules-dev.sh
+++ requirements/upgrade-pymice-modules-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #    This file is part of pymice-analyzer.
-#    Copyright (C) 2018-2020  Emir Turkes
+#    Copyright (C) 2018-2020  Emir Turkes, Phenovance LLC
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
